096928d430537fe53ab6fc7a69c9bed19742b1ff
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-28 23:51:05 -0400

    add appointments namespace

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,8 +2,10 @@ from flask_restx import Api
 
 from src.api.ping import ping_namespace
 from src.api.users.views import users_namespace
+from src.api.appointments.views import appointments_namespace
 
 api = Api(version="1.0", title="Users API", doc="/doc")
 
 api.add_namespace(ping_namespace, path="/ping")
 api.add_namespace(users_namespace, path="/users")
+api.add_namespace(appointments_namespace, path="/appointments")
